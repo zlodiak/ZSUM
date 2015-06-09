5e0ef92c6d183d29af67acd5ed61a66d903609cc
zlodiak <prozaik81-2@yandex.ru>
2015-06-09 15:26:09 +0300

    ok

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+=begin
+  test "create valid user" do
+
+    assert true
+  end
+=end
+
 end
