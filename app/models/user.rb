7fb609e432201f6f831a01264f1f6124674175bc
zlodiak <prozaik81-2@yandex.ru>
2015-06-11 15:03:42 +0300

    logined indikator ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email  = email.downcase  }
+  before_create :create_remember_token
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:     true,
@@ -9,4 +10,18 @@ class User < ActiveRecord::Base
   validates :name,  presence: true, length: { maximum:  50  }
 
   has_secure_password
+
+  def User.new_remember_token
+      SecureRandom.urlsafe_base64
+  end
+  
+  def User.encrypt(token)
+      Digest::SHA1.hexdigest(token.to_s)
+  end  
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end    
 end
