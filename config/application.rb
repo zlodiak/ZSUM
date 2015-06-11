0a9a8bb86aa8c80ba23f660edf6913bebed0a0f3
zlodiak <prozaik81-2@yandex.ru>
2015-06-11 16:50:17 +0300

    local fff

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Zsum
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
+    # config.i18n.available_locales = [:ru, :en]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
