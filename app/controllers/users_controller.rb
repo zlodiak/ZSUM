1e1abceb02aae93d7117735eae90dc103214c3b6
zlodiak <prozaik81-2@yandex.ru>
2015-06-11 10:48:46 +0300

    create user ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
@@ -21,24 +15,16 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params)       # Not the final implementation!
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if  @user.save
+      redirect_to @user
+    else
+      render  'new'
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -51,8 +37,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
@@ -62,13 +46,8 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :session_hash, :phone, :skype, :info, :gender)
+      # params.require(:user).permit(:name, :email, :password, :password_confirmation, :skype, :phone, :info, :gender, )
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
