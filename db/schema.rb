82a3bbc4ee12f3794242313d41686e65beb53410
zlodiak <prozaik81-2@yandex.ru>
2015-06-11 17:45:43 +0300

    posts populate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611114416) do
+ActiveRecord::Schema.define(version: 20150611144235) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
