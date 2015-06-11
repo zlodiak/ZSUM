b18782582d7781af95a7b862698451ef0eaca9fe
zlodiak <prozaik81-2@yandex.ru>
2015-06-11 15:03:42 +0300

    logined indikator ok

---
+++ db/migrate/20150611114416_add_index_to_users.rb
@@ -0,0 +1,5 @@
+class AddIndexToUsers < ActiveRecord::Migration
+  def change
+    add_index   :users, :remember_token
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611113903) do
+ActiveRecord::Schema.define(version: 20150611114416) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -25,5 +25,7 @@ ActiveRecord::Schema.define(version: 20150611113903) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
