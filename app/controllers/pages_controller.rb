888ede5f3a9d931befa8882541b477228776cc7d
Your Name <you@example.com>
2015-06-13 01:21:23 +0300

    markup fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,10 @@ class PagesController < ApplicationController
   end
 
   def popular_diary
+    @popular_diary = User.all.order(views_diary: :DESC)
   end
 
   def lasts_diary
+    @lasts_diary = User.all.order(created_at: :DESC)
   end
 end
