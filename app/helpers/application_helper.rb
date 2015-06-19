12330d209fb1090b31ebe3bae7d9a385b51bdd5c
Your Name <you@example.com>
2015-06-19 20:34:17 +0300

    fff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def lang_switcher
+    content_tag(:ul, class: 'lang-switcher clearfix') do
+      I18n.available_locales.each do |loc|
+        locale_param = request.path == root_path ? root_path(locale: loc) : params.merge(locale: loc)
+        concat content_tag(:li, (link_to loc, locale_param), class: (I18n.locale == loc ? "active" : ""))
+      end
+    end
+  end
 end
