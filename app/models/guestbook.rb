7bcaea7af472cb817cb5c45bb045967e962c0347
zlodiak <prozaik81-2@yandex.ru>
2015-06-19 12:51:13 +0300

    gb worked

--- app/models/guestbook.rb
+++ app/models/guestbook.rb
@@ -1,3 +1,10 @@
 class Guestbook < ActiveRecord::Base
+  before_save { self.email  = email.downcase  }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :name,      presence: true, length: { maximum: 100 }
+  validates :message,   presence: true, length: { maximum: 1000, minimum: 10 }
+
   belongs_to :user
 end
