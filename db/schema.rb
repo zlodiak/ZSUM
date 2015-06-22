e0ce3ef38ae31a2c2332903868961b1c3f8274bd
zlodiak <prozaik81-2@yandex.ru>
2015-06-22 11:43:22 +0300

    def add to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620211933) do
+ActiveRecord::Schema.define(version: 20150622083716) do
 
   create_table "genders", force: :cascade do |t|
     t.string   "gender_name"
@@ -31,10 +31,10 @@ ActiveRecord::Schema.define(version: 20150620211933) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.integer  "user_id"
-    t.integer  "views"
+    t.integer  "views",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
