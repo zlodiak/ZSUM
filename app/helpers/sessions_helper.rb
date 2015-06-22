bfa5ac0f7f1439873a6dd04b0708580b58a1dfce
zlodiak <prozaik81-2@yandex.ru>
2015-06-22 16:15:12 +0300

    view inc not self

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,10 @@ module SessionsHelper
     !current_user.nil?
   end  
 
+  def signed_and_not_self?(user)
+    !current_user.nil? && current_user != user
+  end   
+
   def sign_out
     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
