e39baf3695372d281caa4bc44ccbbb4263a4c778
Your Name <you@example.com>
2015-06-24 22:42:02 +0300

    fff

--- lib/tasks/guestbooks.rake
+++ lib/tasks/guestbooks.rake
@@ -2,23 +2,21 @@ namespace :db do
   desc    "create guestbooks"
   task    populate_guestbooks:   :environment    do    
     30.times  do
-      p 111111
       Guestbook.create(
         name: Faker::Name.name, 
-        email: Faker::Internet.email, 
         message: Faker::Lorem.paragraph(10)
       ) 
 
+=begin
       user = User.find(rand(1..20))
       if user 
-        p 22222222
-        p user.id
         Guestbook.create(
           user_id: user,
-          email: user.email, 
           message: Faker::Lorem.paragraph(10)
         )       
-      end     
+      end 
+=end
+  
     end     
   end
 end
--- test/models/guestbook_test.rb
+++ test/models/guestbook_test.rb
@@ -1,13 +1,23 @@
 require 'test_helper'
 
 class GuestbookTest < ActiveSupport::TestCase
+  def setup
+    @user = User.create(
+                          name: 'namee', 
+                          email: 'adddd@ad.ad', 
+                          diary_name: 'dnndndnnd', 
+                          password: 'qwerty', 
+                          password_confirmation: 'qwerty'
+                        )
+  end
+
   test "should save guestbook for guest user" do
-    guestbook = Guestbook.new(name: 'qwery', email: 'bbbb@ad.ad', message: 'dfsdfsf gdfgfdghfghfghf ghfgh ')
+    guestbook = Guestbook.new(name: 'qwery', message: 'dfsdfsf gdfgfdghfghfghf ghfgh ')
     assert guestbook.save
   end
 
   test "should save guestbook for signin user" do
-    guestbook = Guestbook.new(user_id: 2, email: 'bbbb@ad.ad', message: 'dfsdfsf gdfgfdghfghfghf ghfgh ')
+    guestbook = Guestbook.new(message: 'dfsdfsf gdfgfdghfghfghf ghfgh ', user_id: @user)
     assert guestbook.save
   end  
 end
