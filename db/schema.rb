173c54cc0bfd135c218467cf9b4bba8001c2481f
Your Name <you@example.com>
2015-06-25 21:47:18 +0300

    del views_diary from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625173942) do
+ActiveRecord::Schema.define(version: 20150625184523) do
 
   create_table "genders", force: :cascade do |t|
     t.string   "gender_name"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20150625173942) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "diary_name"
-    t.integer  "views_diary",         default: 0
     t.boolean  "admin",               default: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
