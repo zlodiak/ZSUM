d6eb6d25e76e074209d50c1d5fbcffd0a8c4196f
Your Name <you@example.com>
2015-06-29 15:27:42 +0300

    before scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -30,11 +30,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
-  namespace :admin do
-    resources :users
-    get "admin" => 'users#index'
-  end  
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
