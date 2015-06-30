c67c931dd9501dc94a36cb9ad0b456872b7a1076
Your Name <you@example.com>
2015-06-30 12:33:03 +0300

    before admin nil default

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,17 @@
 module Admin::UsersHelper
+  def get_summary_users
+    @users_count = ::User.all.count
+  end
+
+  def get_summary_posts
+    @posts_count = ::Post.all.count
+  end  
+
+  def get_summary_recalls
+    @recalls_count = ::Recall.all.count
+  end    
+
+  def get_summary_admins
+    @admins_count = ::User.count(admin: true)
+  end  
 end
