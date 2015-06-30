3f24a10c762ab66e3a7e24e35fd94084c8305d1b
Your Name <you@example.com>
2015-06-30 17:31:56 +0300

    img embeded ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def urls_to_images(s)
+    s.gsub! /\s(http:\/\/.*?\.(png|jpg))\s/, 
+      ' <img class="content_image" src="\1" /> '
+    s.html_safe
+  end
+
+  def urls_to_links(s)
+    s.gsub! /\s(http:\/\/.*?)\s/, 
+      ' <a class="content_link" href="\1" target="_blank">\1</a> '
+    s.html_safe    
+  end
 end
