a4b09eb9f768a979c1aeb02b196057a0babfd073
Your Name <you@example.com>
2015-07-03 12:07:16 +0300

    add tags in table ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630093337) do
+ActiveRecord::Schema.define(version: 20150703070634) do
 
   create_table "genders", force: :cascade do |t|
     t.string   "gender_name"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20150630093337) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.integer  "user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "tagname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
