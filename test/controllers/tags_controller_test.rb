47985138108d1295a06aaa626dd1accb26b5f57b
Your Name <you@example.com>
2015-07-05 15:21:24 +0300

    index show ok

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,11 +1,33 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionController::TestCase
-  fixtures :users
+  fixtures :tags
 
   setup do
-    @user = users(:one)  
+    @tag = tags(:one)        
   end
 
-  
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:tags)
+    assert_template :index
+    assert_template layout: "layouts/application"        
+  end  
+
+  test "should get show" do
+    get :show, :id => @tag
+    assert_response :success
+    assert_not_nil assigns(:tag)
+    assert_template :show
+    assert_template layout: "layouts/application"        
+  end    
+
+  test "should route to show tag posts" do
+    assert_generates "/tags/@tag", controller: 'tags', action: 'show', id: "@tag"  # определенный набор опций создает конкретный путь
+  end  
+
+  test "should route to show all tags" do
+    assert_generates "/tags", controller: 'tags', action: 'index'  
+  end    
 end
