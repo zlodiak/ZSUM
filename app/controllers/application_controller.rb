dbec7d9d755ac588d3dadcd250d2f6b7425cdcf9
Your Name <you@example.com>
2015-07-07 22:21:24 +0300

    before refactor find in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,13 @@ class ApplicationController < ActionController::Base
   def default_url_options(options={})
     { locale: I18n.locale }
   end  
+
+  private
+    def render_403
+       render file: "public/403.html", status: 403
+    end
+   
+    def render_404
+       render file: "public/404.html", status: 404
+    end  
 end
